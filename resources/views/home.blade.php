9224c471f3dbaf9b102dee781258d17b6ae8e653
JesseKind <d279759@edu.rocwb.nl>
2022-10-12 10:33:30 +0200

    made style css and fixed images

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 
     <h1>Products</h1>
     <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="img/machine-bit-deluxe.png" alt="Card image cap">
+        <img class="card-img-top" src="{{asset('img/machine-bit-deluxe.png')}}" alt="Card image cap">
         <div class="card-body">
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
                 content.</p>
@@ -13,7 +13,7 @@
     </div>
 
 <div class="card" style="width: 18rem;">
-    <img class="card-img-top" src="img/machine-bit-light.png" alt="Machine Bit Light">
+    <img class="card-img-top" src="{{asset('img/machine-bit-deluxe.png')}}" alt="Machine Bit Light">
     <div class="card-body">
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     </div>
