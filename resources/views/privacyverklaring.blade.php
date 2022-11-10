231e2432bf6dda69baaa38db95aea77269f6ca86
Fonsdoggen <70898032+Fonsdoggen@users.noreply.github.com>
2022-11-10 15:27:56 +0100

    privacyverklaring fix

--- resources/views/privacyverklaring.blade.php
+++ resources/views/privacyverklaring.blade.php
@@ -2,10 +2,8 @@
 
 @section('content')
 
-<h1>Privacyverklaring</h1>
-<p>Privacy verklaring Barroc Intens<br><br>
-
-Barroc Intens respecteert de privacy van al haar klanten en gebruikers van haar site en draagt er zorg voor dat de persoonlijke informatie die u ons verschaft vertrouwelijk wordt behandeld. Dit privacybeleid is van toepassing op alle persoonsgegevens die geregistreerd worden in onze winkel en via onze website. In het geval dat dit privacybeleid wijzigt, vindt u op deze pagina de meest recente informatie.<br><br>
+<h1>Privacyverklaring Barroc Intens</h1>
+<p>Barroc Intens respecteert de privacy van al haar klanten en gebruikers van haar site en draagt er zorg voor dat de persoonlijke informatie die u ons verschaft vertrouwelijk wordt behandeld. Dit privacybeleid is van toepassing op alle persoonsgegevens die geregistreerd worden in onze winkel en via onze website. In het geval dat dit privacybeleid wijzigt, vindt u op deze pagina de meest recente informatie.<br><br>
 
 Registratie persoonsgegevens<br>
 Barroc Intens gebruikt de verzamelde gegevens om haar klanten de volgende diensten te leveren: Als u een bestelling plaatst in de winkel, telefonisch of per mail, hebben we uw naam, e-mailadres, afleveradres nodig om uw bestelling uit te voeren en u van het verloop daarvan op de hoogte te houden. Verder slaan we via de website gegevens op over het gebruik van onze site. De feedback die we op deze manier krijgen van onze bezoekers, helpen ons om onze site verder te ontwikkelen en te verbeteren.<br><br>
@@ -17,7 +15,7 @@ Barroc Intens verkoopt uw gegevens niet<br>
 Barroc Intens zal uw persoonlijke gegevens niet aan derden verkopen en zal deze uitsluitend aan derden ter beschikking stellen die zijn betrokken bij het uitvoeren van uw bestelling. Onze werknemers en de door ons ingeschakelde derden zijn verplicht om de vertrouwelijkheid van uw gegevens te respecteren.<br><br>
 
 Contactadres<br>
-Alle vragen of opmerkingen over ons privacy beleid en de wijze waarop wij gegevens verzamelen en bewaren, verzoeken tot toezending, inzage, aanpassing of verwijdering van uw persoonsgegevens, beperking van de verwerking van uw persoonsgegevens en overdracht van uw persoonsgegevens kunt u richten aan:<br>
+Alle vragen of opmerkingen over ons privacy beleid en de wijze waarop wij gegevens verzamelen en bewaren, verzoeken tot toezending, inzage, aanpassing of verwijdering van uw persoonsgegevens, beperking van de verwerking van uw persoonsgegevens en overdracht van uw persoonsgegevens kunt u richten aan:<br><br>
 
 Barroc Intens<br>
 Terheijdenseweg 350<br>
