174628d25e1357ae5d57d113fd53c6a0a453ecd6
Fonsdoggen <70898032+Fonsdoggen@users.noreply.github.com>
2022-11-10 15:26:23 +0100

    privacyverklaring

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -173,13 +173,12 @@
                 <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
                     <!-- Links -->
                     <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
-                    <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
+                    <p><i class="fas fa-home me-3"></i>4826 AA Breda</p>
                     <p>
                         <i class="fas fa-envelope me-3"></i>
-                        info@example.com
+                        info@barrocintens.nl
                     </p>
-                    <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
-                    <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+                    <p><i class="fas fa-phone me-3"></i>+31(0)76 - 5733444</p>
                 </div>
                 <!-- Grid column -->
             </div>
--- routes/web.php
+++ routes/web.php
@@ -38,8 +38,7 @@ Route::resource('factuur', InvoiceController::class);
 Route::resource('quotation', QuotationController::class);
 
 Route::get('/privacyverklaring', function() {
-    return view('privacyverklaring');
-})->name('privacyverklaring');
+    return view('privacyverklaring');})->name('privacyverklaring');
 // Dashboard
 
 Route::get('/dashboard', function () {
